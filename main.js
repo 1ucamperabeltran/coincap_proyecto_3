bca2c9a5b1e7aa93a60860c6d81995b34be21fca
1ucamperabeltran <arbeltran@hotmail.com>
2023-09-19 22:38:12 -0500

    agregar nuevas columnas y formatear numeros

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const tblValores = document.getElementById("tblValores")
-
+//obtener el elemento tabla 
 function cargarDatos() {
+    //obtener elemento grafica 
     const grafica = document.getElementById("myChart").getContext("2d")
 
-    axios.get("https://api.coincap.io/v2/assets").then(resultado => {
+    axios.get("https://api.coincap.io/v2/assets")
+        .then(resultado => {
+        // escribir los resultados en la consola            
         console.log(resultado);
-        //Las etiquetas de los valores obtenidos   
+
+        //Las etiquetas son los simbolos de cada moneda. Ejemplo Bitcoin --> BTC
         const etiquetas = resultado.data.data.map(item => {
             return item.symbol.toUpperCase();
         })
 
+        console.log(etiquetas)
+        
         //Los valores de comparacion
         const costos = resultado.data.data.map(item => {
             return parseInt(item.priceUsd)
         })
+        
+        console.log(costos)
 
         const myChart = new Chart(grafica, {
             type: "line",
@@ -50,19 +58,49 @@ function cargarDatos() {
 
 
         tblValores.innerHTML = "";
-        for (const valor of resultado.data.data) {
-            if (parseInt(valor.priceUsd) > 100) {
+        const data = resultado.data.data.
+            sort((a, b)=> parseInt(a.rank) - parseInt(b.rank))
+        for (const valor of data) {
                 let tr = `<tr>
-                <td>${valor.symbol}</td>
-                <td>${valor.name}</td>
-                <td>${parseInt(valor.priceUsd)}</td>
+                <td>${parseInt(valor.rank)}</td>
+                <td>${valor.symbol} - ${valor.name}</td>
+                <td>${formatNumero(valor.priceUsd)}</td>
+                <td>${formatNumero(valor.marketCapUsd)}</td>
+                <td>${formatNumero(valor.vwap24Hr)}</td>
+                <td>${formatNumero(valor.supply)}</td>
+                <td>${formatNumero(valor.volumeUsd24Hr)}</td>
+                <td>${parseFloat(valor.changePercent24Hr).toFixed(2)}%</td>
                 </tr> `
                 tblValores.innerHTML += tr;
-            }
-
         }
     })
 
 }
+
+function formatNumero(numeroStr){
+    let numero = parseFloat(numeroStr)
+
+    if (numero < 1000000)
+        return convertirNumeroDolar(numero)
+    
+    if(numero > 1000000 && numero < 1000000000){
+        numero = numero / 1000000
+        return convertirNumeroDolar(numero) + 'm'
+    }
+    
+    if(numero > 1000000000){
+        numero = numero / 1000000000
+        return convertirNumeroDolar(numero) + 'b'
+    }
+}
+
+function convertirNumeroDolar(numero){
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(numero);
+}
+
+
 
 cargarDatos()
